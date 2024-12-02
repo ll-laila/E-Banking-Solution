327baa4b61009d7c566c710be2d4ab9a7abb9730
Chaimae Ouazzan <OU-chaima.ouazzanchaimae@gmail.com>
2024-12-02 18:36:03 +0100

    first-commit

---
+++ BankatiProject_Backend/services/carte-virtuelle/src/main/java/org/example/cartevirtuelle/CarteVirtuelleApplication.java
@@ -0,0 +1,13 @@
+package org.example.cartevirtuelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteVirtuelleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarteVirtuelleApplication.class, args);
+    }
+
+}
