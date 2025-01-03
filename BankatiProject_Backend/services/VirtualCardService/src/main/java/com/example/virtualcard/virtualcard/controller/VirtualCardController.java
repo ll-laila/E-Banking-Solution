4739000e41a9070c5db1eadf4393e8e19a708c09
Salwahnid <salwahnid1@gmail.com>
2025-01-03 18:27:37 +0100

    pull

--- BankatiProject_Backend/services/VirtualCardService/src/main/java/com/example/virtualcard/virtualcard/controller/VirtualCardController.java
+++ BankatiProject_Backend/services/VirtualCardService/src/main/java/com/example/virtualcard/virtualcard/controller/VirtualCardController.java
@@ -16,25 +16,21 @@ public class VirtualCardController {
     @Autowired
     private VirtualCardService virtualCardService;
 
-    // Créer une carte virtuelle
     @PostMapping("/create/{userId}")
     public VirtualCard createCard(@PathVariable String userId) {
         return virtualCardService.createVirtualCard(userId);
     }
 
-    // Activer une carte virtuelle
     @PatchMapping("/activate/{cardId}")
     public VirtualCard activateCard(@PathVariable String cardId) {
         return virtualCardService.activateCard(cardId);
     }
 
-    // Désactiver une carte virtuelle
     @PatchMapping("/deactivate/{cardId}")
     public VirtualCard deactivateCard(@PathVariable String cardId) {
         return virtualCardService.deactivateCard(cardId);
     }
 
-    // Obtenir toutes les cartes d'un utilisateur
     @GetMapping("/user/{userId}")
     public List<VirtualCard> getCardsByUser(@PathVariable String userId) {
         return virtualCardService.getCardsByUserId(userId);
